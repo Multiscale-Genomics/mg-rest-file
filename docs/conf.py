2c3bf721acc5a9aafbb3a45704545bc97c4365e1
Mark McDowall <mark.mcdowall@gmail.com>
2017-10-31 13:57:40 +0000

    Tried removing the mg-rest-util and then mocking the module instead

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['pyBigWig', 'dmp']
+MOCK_MODULES = ['pyBigWig', 'dmp', 'mg-rest-util']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
