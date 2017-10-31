900aa0b53059023a00aecb7c36bce03fa6ef04fa
Mark McDowall <mark.mcdowall@gmail.com>
2017-10-31 14:20:08 +0000

    Use of functools has fixed the loading of decorated functions

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['pyBigWig', 'dmp', 'mg-rest-util']
+MOCK_MODULES = ['pyBigWig', 'dmp']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
